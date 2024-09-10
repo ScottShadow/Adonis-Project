070c52935e5ba995784f850b2af15631e20da431
Rudaseswa Scott Mc Davis <128939673+ScottShadow@users.noreply.github.com>
2024-09-10 18:13:53 +0300

    Update tag_views.py

--- api/v2/views/tag_views.py
+++ api/v2/views/tag_views.py
@@ -1,6 +1,8 @@
+from flask import jsonify, Blueprint
+from sqlalchemy.orm import Session
+from models.base import SessionLocal
 from models.tag import Tag
 from models.user import User
-from flask import jsonify, Blueprint
 
 tag_views = Blueprint('tag_views', __name__, url_prefix="/api/v2")
 
@@ -8,8 +10,9 @@ tag_views = Blueprint('tag_views', __name__, url_prefix="/api/v2")
 @tag_views.route('/user/<user_id>/tags', methods=['GET'])
 def get_user_tags(user_id):
     """Retrieves tags for a given user"""
+    session = SessionLocal()
     try:
-        user = User.query.get(user_id)
+        user = session.query(User).get(user_id)
         if not user:
             return jsonify({"error": "User not found"}), 404
         # Retrieve tags for the user
@@ -28,22 +31,53 @@ def get_user_tags(user_id):
         return jsonify({"tags": tags_data}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    finally:
+        session.close()
 
 
 @tag_views.route('/tags', methods=['GET'])
 def get_tags():
     """Retrieve all tags."""
-    tags = Tag.query.all()
-    tags_list = [{'id': tag.id, 'name': tag.name, 'description': tag.description, 'level': tag.level, 'category': tag.category} for tag in tags]
-    return jsonify(tags_list)
+    session = SessionLocal()
+    try:
+        tags = session.query(Tag).all()
+        tags_list = [
+            {
+                'id': tag.id,
+                'name': tag.name,
+                'description': tag.description,
+                'level': tag.level,
+                'category': tag.category
+            }
+            for tag in tags
+        ]
+        return jsonify(tags_list)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        session.close()
 
 
 @tag_views.route('/tags/category/<category>', methods=['GET'])
 def get_tags_by_category(category):
     """Retrieve tags by category."""
-    tags = Tag.query.filter_by(category=category).all()
-    tags_list = [{'id': tag.id, 'name': tag.name, 'description': tag.description, 'level': tag.level, 'category': tag.category} for tag in tags]
-    return jsonify(tags_list)
+    session = SessionLocal()
+    try:
+        tags = session.query(Tag).filter_by(category=category).all()
+        tags_list = [
+            {
+                'id': tag.id,
+                'name': tag.name,
+                'description': tag.description,
+                'level': tag.level,
+                'category': tag.category
+            }
+            for tag in tags
+        ]
+        return jsonify(tags_list)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        session.close()
 
-if __name__ == '__main__':
-    tag_views.run(debug=True)
+
