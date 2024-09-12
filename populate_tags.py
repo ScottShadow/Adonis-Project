4bea4c123b13561f6d8f2133638f815f45d74a90
Kamikazi <msk.megbenu@gmail.com>
2024-09-12 06:29:03 +0200

    added some predertermined tags

--- populate_tags.py
+++ populate_tags.py
@@ -7,11 +7,34 @@ from models.tag import Tag
 def seed_tags():
     # Initialize database session
     session = SessionLocal()
-     try:
+    try:
         # Define tags to be added
         tags = [
-            Tag(id='1', name='gym shark', description='Fitness enthusiast', level=1, category='self improvement'),
-            Tag(id='2', name='bibliophile', description='Lover of books', level=1, category='self improvement'),
+            Tag(id='1', name='Gym Shark', description='Your body is your temple!', level=1, category='Exercise and Fitness'),
+            Tag(id='2', name='Athlete', description='Fitness enthusiast', level=1, category='Exercise and Fitness'),
+            Tag(id='3', name='Health Freak', description='Passionate about healthy living', level=1, category='Exercise and Fitness'),
+            Tag(id='4', name='Body Builder', description='Live for the gains', level=1, category='Exercise and Fitness'),
+            Tag(id='5', name='Sport King', description='Competition is your drug', level=1, category='Exercise and Fitness'),
+
+            Tag(id='6', name='Bibliophile', description='Lover of books', level=1, category='Learning and Education'),
+            Tag(id='7', name='Scholar', description='Chasing that knowledge!', level=1, category='Learning and Education'),
+            Tag(id='8', name='Programmer', description='Lover of books', level=1, category='Learning and Education'),
+            Tag(id='9', name='Scholar', description='Lover of books', level=1, category='Learning and Education'),
+            Tag(id='10', name='Scholar', description='Lover of books', level=1, category='Learning and Education'),
+            Tag(id='11', name='Scholar', description='Lover of books', level=1, category='Learning and Education'),
+
+            Tag(id='12', name='Social Butterfly', description='People are your power!', level=1, category='Social Interaction'),
+            Tag(id='13', name='Life of the Party', description='Never a dull time around you!', level=1, category='Social Interaction'),
+            Tag(id='14', name='Mediator', description='You bring people together!', level=1, category='Social Interaction'),
+            Tag(id='15', name='Introvert', description='Time alone is time well spent!', level=1, category='Social Interaction'),
+            Tag(id='16', name='Night Owl', description='You live for the nightlife!', level=1, category='Social Interaction'),
+
+            Tag(id='17', name='Code Junkie', description='Eat.Sleep.Code.Repeat.', level=1, category='Other'),
+            Tag(id='18', name='Teacher', description='Lover of books', level=1, category=''),
+            Tag(id='19', name='Lover', description='Lover of books', level=1, category='Other'),
+            Tag(id='20', name='Friend', description='Lover of books', level=1, category='Other'),
+            Tag(id='21', name='Good Guy', description='Lover of books', level=1, category='Other'),
+
             # Add more tags as needed
         ]
 
