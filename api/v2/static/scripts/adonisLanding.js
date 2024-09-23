748b0c446552701960eb8026b9fc1f12f6ca8acc
Kamikazi <msk.megbenu@gmail.com>
2024-09-24 01:49:23 +0200

    popup modal fixed

--- api/v2/static/scripts/adonisLanding.js
+++ api/v2/static/scripts/adonisLanding.js
@@ -3,15 +3,6 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
-  // Activate Bootstrap scrollspy on the main nav element
-  const mainNav = document.body.querySelector('#mainNav');
-  if (mainNav) {
-    new bootstrap.ScrollSpy(document.body, {
-      target: '#mainNav',
-      offset: 74,
-    });
-  };
-
   // Collapse responsive navbar when toggler is visible
   const navbarToggler = document.body.querySelector('.navbar-toggler');
   const responsiveNavItems = [].slice.call(
