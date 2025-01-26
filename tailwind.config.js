542b5829a4c8e85fe7f8878057ec95f6571e8a30
ScottShadow <rudaseswascottmc@gmail.com>
2025-01-26 21:59:07 +0200

    build custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        // Extend colors if needed
+      },
+    },
+  },
+  safelist: [
+    {
+      pattern: /theme-(dark|standard|midnight|pastel|lagoon|serenity)/,
+    },
+  ],
+};
